14c7eef3acd7a120824b946894204af47d0c6492
Ali Zahid <ali.zahid@live.com>
2020-07-07 10:02:25 +0400

    Refactoring

--- src/components/refresher.tsx
+++ src/components/refresher.tsx
@@ -1,8 +1,11 @@
+import color from 'color'
 import React, { FunctionComponent } from 'react'
 import { RefreshControl, RefreshControlProps } from 'react-native'
 
 import { colors } from '../styles'
 
+const tintColor = color(colors.primary).lighten(0.5).hex()
+
 export const Refresher: FunctionComponent<RefreshControlProps> = (props) => (
-  <RefreshControl {...props} tintColor={colors.primary} />
+  <RefreshControl {...props} tintColor={tintColor} />
 )
