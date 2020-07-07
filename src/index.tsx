d863ce65064c2aa2be96d15cb797eb1cf6cf8f17
Ali Zahid <ali.zahid@live.com>
2020-07-07 11:09:54 +0400

    Bug fix

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,22 @@ import { ApolloProvider } from '@apollo/react-hooks'
 import { NavigationContainer } from '@react-navigation/native'
 import React, { FunctionComponent, useEffect } from 'react'
 
-import { KeyboardView } from './components'
+import { KeyboardView, Spinner } from './components'
 import { client } from './graphql'
 import { AppNavigator, AuthNavigator } from './navigators'
 import { useAuth } from './store'
 import { theme } from './styles'
 
 export const Blender: FunctionComponent = () => {
-  const [{ loggedIn }, { init }] = useAuth()
+  const [{ loading, loggedIn }, { init }] = useAuth()
 
   useEffect(() => {
     init()
   }, [init])
+
+  if (loading) {
+    return <Spinner full />
+  }
 
   return (
     <ApolloProvider client={client}>
