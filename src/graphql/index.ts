70ae108e20f86d33c5d49af403140f86ed6dfffd
Ali Zahid <ali.zahid@live.com>
2020-07-13 01:06:13 +0400

    Sign out on 401

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -5,6 +5,7 @@ import ApolloClient, {
 } from 'apollo-boost'
 import { API_URI } from 'react-native-dotenv'
 
+import { mitter } from '../lib'
 import schema from './schema.json'
 
 export const client = new ApolloClient({
@@ -13,6 +14,11 @@ export const client = new ApolloClient({
       introspectionQueryResultData: schema
     })
   }),
+  onError(error) {
+    if (error.response?.errors?.[0].extensions?.code === 401) {
+      mitter.emit('logout')
+    }
+  },
   async request(operation) {
     const token = await AsyncStorage.getItem('@token')
 
--- src/index.tsx
+++ src/index.tsx
@@ -5,16 +5,27 @@ import { SafeAreaProvider } from 'react-native-safe-area-context'
 
 import { Dialog, KeyboardView, Spinner } from './components'
 import { client } from './graphql'
+import { mitter } from './lib'
 import { AppNavigator, AuthNavigator } from './navigators'
 import { useAuth } from './store'
 import { theme } from './styles'
 
 export const Blender: FunctionComponent = () => {
-  const [{ loading, loggedIn }, { init }] = useAuth()
+  const [{ loading, loggedIn }, { init, logout }] = useAuth()
 
   useEffect(() => {
     init()
   }, [init])
+
+  useEffect(() => {
+    const handler = () => logout()
+
+    mitter.on('logout', handler)
+
+    return () => {
+      mitter.off('logout', handler)
+    }
+  }, [logout])
 
   if (loading) {
     return <Spinner full />
