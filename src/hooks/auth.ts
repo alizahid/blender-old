c475b219998bfeff4d119b7c5a345019b1f38951
Ali Zahid <ali.zahid@live.com>
2020-07-13 01:06:39 +0400

    UX tweak

--- src/hooks/auth.ts
+++ src/hooks/auth.ts
@@ -71,6 +71,7 @@ export const useSignIn = () => {
             await login(idToken, user.id, user.email)
           } else if (user?.otpEnabled) {
             const code = await dialog.prompt({
+              keyboardType: 'number-pad',
               message: 'Enter your 6-digit two-factor code',
               placeholder: 'Code',
               title: 'Two-factor auth'
